ad8fb2282da6a4c6e027f580fcff1fabcf228f1f
shrmp <159952243+surtricecream@users.noreply.github.com>
2025-08-22 17:23:48 +0100

    Main Header

---
+++ header.h
@@ -0,0 +1,26 @@
+#ifndef header
+#define header
+
+/**
+ * @file header.h
+ * @author Francisco Simões (ist1113780)
+ * @brief Header file for the project.
+ *
+ * This file includes all necessary headers and defines the structure
+ * of the project. It serves as the main entry point for including
+ * project-wide dependencies.
+*/
+
+/**< Library includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**< Files includes */
+#include "constants.h"      /**< Contains project constants. */
+#include "structs.h"        /**< Defines custom structs used in the project. */
+#include "functions.h"      /**< Declares functions used throughout the project. */
+#include "auxfunctions.h"   /**< Declares auxiliary functions used in the project. */
+
+#endif
